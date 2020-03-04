981c40a77d6309003e258d4b0dd7af26931b9a78
Amira <amira071846@feng.bu.edu.eg>
2020-03-04 14:41:23 +0200

    basic datatypes second part

---
+++ strings.py
@@ -0,0 +1,38 @@
+#In most fo the programmming languages a b c d declared as char and write declare as string
+#but in python all  a b c d write declared as string
+
+x = "c"
+print(type(x))
+
+x = "write"
+print(type(x))
+
+
+#python understand string that is the data between quotes "" or ''
+x= 2
+print(type(x))
+
+x = "2"
+print(type(x))
+
+
+#use the method to convert text from lower to upper case
+#method are function related to a class like upper related to string
+x = "water"
+print(x.upper())
+
+# to get all method related to datatype write dir("datatype")
+dir("string")
+print(dir("string"))
+
+#to get the first char capital
+print(x.title())
+
+#to convert text from capiy]talized to lower
+x= "WATER"
+print(x.lower())
+
+#remove space from string
+y = "  value"
+print(y)
+print(y.split())
