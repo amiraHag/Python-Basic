3d064c93bbf31dc45a60b3ec36b722a4a434f46a
Amira <amira071846@feng.bu.edu.eg>
2020-03-07 16:37:40 +0200

    operators second part

--- operators/arithmatic-operators.py
+++ operators/arithmatic-operators.py
@@ -1,2 +1,45 @@
 #arithmatic operators perform arithmatic calculations in any particular sentex
 # + , - , * , / , % , ** , //
+#addition operator + : perform the addition process in two different values
+x= "amira"
+y="HM"
+z= x+y
+print(z)
+
+#subtraction operator - : perform the subtraction process in two different values
+a = 9
+b = 3
+c= a-b
+print(c)
+
+#subtraction operator can not be applied to strings
+# z=x-y will get an error
+
+#multiplied oprator * : perform the multiplication process in two different values
+#unexpected indent is kind of error that appear if you left space in the start of the sentence or in any place that it shouldn't be space on it
+# a="cc" issue an error
+a= 8
+b= 5
+c = 8*5
+print(c)
+#division operator / : perform the division process in two different values
+a = 12
+b = 3
+c = a/b
+print(c)
+# double division operator // : perform the division process -with round the result-  in two different values
+a= 10
+b= 3
+c= a/b
+d= a//b
+print(c)
+print(d)
+
+#reminder operator % : the result is the reminder of the division process in two different values
+#the result is the left portion of the division operation
+e= a%b
+print(e)
+
+#power operator ** : perform the power operation in two different values
+v= a**b
+print(v)
---
+++ operators/assigment-operators.py
@@ -0,0 +1,32 @@
+#assigment operators: use to assign any value to anything
+#equal operator = : assign value to variable
+x = 15
+print(x)
+
+#increment operator += : increment the variable by specific value
+x +=5
+print(x)
+
+#decrement operator -= : decrement the variable by specific value
+x -=5
+print(x)
+
+#mu;tiply operator *= : multiply the variable by specific value
+x *=5
+print(x)
+
+#division operator /= : divide the variable by specific value
+x /=5
+print(x)
+
+#reminder operator %= : the result is the reminder of the division of variable with the given value
+x %=2
+print(x)
+
+#power operator **= : power the variable by specific value
+x **=5
+print(x)
+
+#double division operator //= : the result is the int value of the division of the variable by the given value
+x //=5
+print(x)
