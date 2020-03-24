4a8bbca145cdab1101f676eb3dd2a43f7064c214
Amira <amira071846@feng.bu.edu.eg>
2020-03-24 18:39:24 +0200

    operators third part

---
+++ operators/comparison-operators.py
@@ -0,0 +1,33 @@
+#Comparison operators compare two diffrent variables
+# comprision operators: == , != , > , >= , < , <=
+x=3
+y=3
+z=5
+
+# equal to operator == : compare if the two variables are the same and the result is true or false
+print(x==y)
+print(x==z)
+print("*******")
+
+# not equal to operator != : is the opposite of ==
+print(x!=z)
+print("*******")
+
+# greater than operator > : compare if variable value greater than other variable value
+print(z>x)
+print(y>x)
+print("*******")
+
+# greater than operator or equal >= : compare if variable value greater than or equal other variable value
+print(z>=x)
+print(y>=x)
+print("*******")
+
+# less than operator < : compare if variable value less than other variable value
+print(x<z)
+print(x<y)
+print("*******")
+
+# less than operator or equal <= : compare if variable value less than or equal other variable value
+print(x<=z)
+print(x<=y)
